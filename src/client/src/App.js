83f03eea8793b512a986d20df10e653fa488d4f8
MichaelDjab <mich.dja@gmail.com>
2022-02-07 09:59:57 -0500

    Establish connection between server/client and create header navbar skeleton

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react';
+import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          Hello World!
-        </p>
-        
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }))
+          .catch(err => err);
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <p>Hello world from client!</p>
+        <p className="App-intro">{this.state.apiResponse}</p>
+        <Header/>
+      </div>
+    );
+  }
 }
-
 export default App;
