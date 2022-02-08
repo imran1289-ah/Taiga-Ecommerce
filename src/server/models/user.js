da78f0e1c7d5d42ed395dd49edef9bd523c549e8
Matthew Faigan <matthew.faigan@gmail.com>
2022-02-08 16:43:28 -0500

    Install mongoose and create user model

---
+++ src/server/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema(
+    {
+        email: {type: String, required: true},
+        name: {type: String, required: true},
+        hashedPassword: {type: String, required: true},
+        salt: {type: String, required: true},
+        userType: {type: String, enum: ['customer', 'seller', 'admin']},
+        address: String,
+        creditCard: String
+    }
+);
+
+module.exports = mongoose.model('User', UserSchema);
