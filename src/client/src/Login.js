83dba871ecd874e33635cf8318db89c31dc41597
MichaelDjab <mich.dja@gmail.com>
2022-02-13 09:05:52 -0500

    Modify Login and add SignUp
    Change login to more modern design, add sign up form

--- src/client/src/Login.js
+++ src/client/src/Login.js
@@ -8,29 +8,31 @@ function Login() {
     const [password, setPassword] = useState('');
 
     return (
-      <div class = "mainform">
-        <form>
-        <h3>Log In</h3>
-        <div className="form-group">
-            <label>Email address</label>
-            <input type="email" className="form-control" placeholder="Enter email" />
-        </div>
-        <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Enter password" />
-        </div>
-        <div className="form-group">
-            <div className="custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+            <div class="main">  	
+                <input type="checkbox" id="chk" aria-hidden="true"></input>
+        
+                    <div class="signup">
+                        <form>
+                            <label for="chk" aria-hidden="true">Sign up</label>
+                            <input class="login-signup-input" type="text" name="txt" placeholder="User name" required=""></input>
+                            <input class="login-signup-input" type="email" name="email" placeholder="Email" required=""></input>
+                            <input class="login-signup-input" type="password" name="pswd" placeholder="Password" required=""></input>
+                            <button class="login-signup-button">Sign up</button>
+                        </form>
+                    </div>
+                    <div>
+                        
+                    </div>
+        
+                    <div class="login">
+                        <form>
+                            <label for="chk" aria-hidden="true">Login</label>
+                            <input class="login-signup-input" type="email" name="email" placeholder="Email" required=""></input>
+                            <input class="login-signup-input" type="password" name="pswd" placeholder="Password" required=""></input>
+                            <button class="login-signup-button">Login</button>
+                        </form>
+                    </div>
             </div>
-        </div>
-        <button type="submit" className="btn btn-primary btn-block">Submit</button>
-        <p className="forgot-password text-right">
-            Forgot <a href="#">password?</a>
-        </p>
-      </form>
-      </div>
     )
 }
 
