a02c9a92ffe8e15c4dd86d4803bfc891cceda35e
Matthew Faigan <matthew.faigan@gmail.com>
2022-02-13 14:59:12 -0500

    Finish setup of passport authentication

--- src/server/index.js
+++ src/server/index.js
@@ -18,6 +18,8 @@ app.use(cors());
 mongoose.connect('mongodb://localhost:27017/taiga');
 const connection = mongoose.connection;
 
+const UserModel = require('./models/user');
+
 // Set up connect-mongo and express-session to track user login sessions
 const sessionStore = new MongoStore({ mongooseConnection: connection, collection: 'sessions' });
 app.use(session({
@@ -27,11 +29,13 @@ app.use(session({
     store: sessionStore
 }));
 
+// Function to validate a password during login requests
 function validatePassword(password, hash, salt) {
     // Create a hashed password from the user input and compare it to the hash stored in the database
     var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
     return hash === hashVerify;
 }
+// Function to generate a salt and hash for a new password on registration or password change
 function generateNewPassword(password) {
     // Generate a random salt for the user's password then compute the hash
     var salt = crypto.randomBytes(32).toString('hex');
@@ -39,6 +43,49 @@ function generateNewPassword(password) {
 
     return { salt: salt, hash: hash };
 }
+// Tell passport how it should validate a login attempt
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function (email, password, callback) {
+        // Find a user in the DB with the supplied email address
+        UserModel.findOne({ email: email })
+            .then(user => {
+                // No user found, call the callback function with no error and no user
+                if (!user) { return callback(null, false) }
+
+                // User found, validate the supplied password
+                if (validatePassword(password, user.hash, user.salt)) {
+                    // Correct password, call the callback function with no error and the found user
+                    return callback(null, user);
+                }
+                else {
+                    // User found but password incorrect, call the callback function with no error and no user
+                    return callback(null, false);
+                }
+            })
+            .catch(err => {
+                callback(err);
+            });
+    }
+));
+// Tell passport how it should represent a user in the session store (i.e., by its uniquely generated ID)
+passport.serializeUser(function (user, callback) {
+    callback(null, user.id)
+});
+// Tell passport how it should go from the session store back to a full user object
+passport.deserializeUser(function (id, callback) {
+    UserModel.findById(id, function (err, user) {
+        if (err) { return callback(err); }
+        callback(null, user);
+    });
+});
+
+// Finally, add passport middleware to the express app for authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.send('Hello world from server!');
