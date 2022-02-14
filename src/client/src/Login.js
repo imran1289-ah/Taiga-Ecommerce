17eb5362f9e465c11ac443b468de12c7cac38d54
MichaelDjab <mich.dja@gmail.com>
2022-02-14 12:24:18 -0500

    Modify look and feel of login

--- src/client/src/Login.js
+++ src/client/src/Login.js
@@ -11,25 +11,30 @@ function Login() {
             <div class="main">  	
                 <input type="checkbox" id="chk" aria-hidden="true"></input>
         
-                    <div class="signup">
-                        <form>
-                            <label for="chk" aria-hidden="true">Sign up</label>
-                            <input class="login-signup-input" type="text" name="txt" placeholder="User name" required=""></input>
-                            <input class="login-signup-input" type="email" name="email" placeholder="Email" required=""></input>
-                            <input class="login-signup-input" type="password" name="pswd" placeholder="Password" required=""></input>
-                            <button class="login-signup-button">Sign up</button>
-                        </form>
-                    </div>
-                    <div>
-                        
-                    </div>
-        
                     <div class="login">
                         <form>
                             <label for="chk" aria-hidden="true">Login</label>
                             <input class="login-signup-input" type="email" name="email" placeholder="Email" required=""></input>
                             <input class="login-signup-input" type="password" name="pswd" placeholder="Password" required=""></input>
                             <button class="login-signup-button">Login</button>
+                        </form>
+                    </div>
+
+                    <div class="signup">
+                        <form>
+                            <label for="chk" aria-hidden="true">Sign up</label>
+                            <input class="login-signup-input" type="email" name="email" placeholder="Email" required=""></input>
+                            <input class="login-signup-input" type="text" name="txt" placeholder="Name" required=""></input>
+                            <input class="login-signup-input" type="password" name="pswd" placeholder="Password" required=""></input>
+                            <input class="login-signup-input" type="address" name="address" placeholder="Address" required=""></input>
+                            <input class="login-signup-input" type="password" name="CreditCardNumber" placeholder="Credit Card Number" required=""></input>
+                            <select class = "login-signup-select">
+                                <option value="customer">Customer</option>
+                                <option value="seller">Seller</option>
+                                <option value="admin">Admin</option>
+                            </select>
+
+                            <button class="login-signup-button">Sign up</button>
                         </form>
                     </div>
             </div>
