76199820396ef264107d657af2f704bf6f7f7e87
ZiHao-Tan <77635831+ZiHao-Tan@users.noreply.github.com>
2022-02-16 16:55:24 -0500

    Implemented list of products for each catertory.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,62 +1,301 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SlideShow from './SlideShow';
-import "./App.css";
+import './App.css';
 import Types from './Types';
 import Footer from './Footer';
 import FurnitureTypes from './FurnitureTypes';
 import BooksTypes from './BooksTypes';
 import GroceryTypes from './GroceryTypes';
 import ToysTypes from './ToysTypes';
+import MainGameContent from './MainGameContent';
+import MainPhoneContent from './MainPhoneContent';
+import MainComputerContent from './MainComputerContent';
+import MainMenClothesContent from './MainMenClothesContent';
+import MainWomenClothesContent from './MainWomenClothesContent';
+import MainKidsClothesContent from './MainKidsClothesContent';
+import MainLivingFurnitureContent from './MainLivingFurnitureContent';
+import MainBedroomFurnitureContent from './MainBedroomFurnitureContent';
+import MainNovelsContent from './MainNovelsContent';
+import MainTextbooksContent from './MainTextbooksContent';
+import MainComicsContent from './MainComicsContent';
+import MainFruitsContent from './MainFruitsContent';
+import MainVegetablesContent from './MainVegetablesContent';
+import MainSnacksContent from './MainSnacksContent';
+import MainBoyToysContent from './MainBoyToysContent';
+import MainGirlToysContent from './MainGirlToysContent';
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
-import Electronics_Types from './ElectronicsTypes';
+//import Electronics_Types from "./ElectronicsTypes";
 import ElectronicsTypes from './ElectronicsTypes';
 import ClothesTypes from './ClothesTypes';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
+    this.state = { apiResponse: '' };
   }
 
   callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }))
-          .catch(err => err);
+    fetch('http://localhost:9000/testAPI')
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }))
+      .catch((err) => err);
   }
 
   componentWillMount() {
-      this.callAPI();
+    this.callAPI();
   }
 
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ 
-            /*<p className="App-intro">{this.state.apiResponse}</p>*/
-            <><Header/><SlideShow/><Types/><Footer/></>
-        } />
-          <Route path="/login" element={<><Header/><Login/></>} />
-          <Route path="/ElectronicsTypes" element={<><Header/><ElectronicsTypes/><Footer/></>}></Route>
-          <Route path="/ClothesTypes" element={<><Header/><ClothesTypes/><Footer/></>}></Route>
-          <Route path="/FurnitureTypes" element={<><Header/><FurnitureTypes/><Footer/></>}></Route>
-          <Route path="/BooksTypes" element={<><Header/><BooksTypes/><Footer/></>}></Route>
-          <Route path="/GroceryTypes" element={<><Header/><GroceryTypes/><Footer/></>}></Route>
-          <Route path="/ToysTypes" element={<><Header/><ToysTypes/><Footer/></>}></Route>
+          <Route
+            path="/"
+            element={
+              /*<p className="App-intro">{this.state.apiResponse}</p>*/
+              <>
+                <Header />
+                <SlideShow />
+                <Types />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                <Header />
+                <Login />
+              </>
+            }
+          />
+          <Route
+            path="/ElectronicsTypes"
+            element={
+              <>
+                <Header />
+                <ElectronicsTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/ClothesTypes"
+            element={
+              <>
+                <Header />
+                <ClothesTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/FurnitureTypes"
+            element={
+              <>
+                <Header />
+                <FurnitureTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/BooksTypes"
+            element={
+              <>
+                <Header />
+                <BooksTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/GroceryTypes"
+            element={
+              <>
+                <Header />
+                <GroceryTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/ToysTypes"
+            element={
+              <>
+                <Header />
+                <ToysTypes />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainGameContent"
+            element={
+              <>
+                <Header />
+                <MainGameContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainPhoneContent"
+            element={
+              <>
+                <Header />
+                <MainPhoneContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainComputerContent"
+            element={
+              <>
+                <Header />
+                <MainComputerContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainMenClothesContent"
+            element={
+              <>
+                <Header />
+                <MainMenClothesContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainWomenClothesContent"
+            element={
+              <>
+                <Header />
+                <MainWomenClothesContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainKidsClothesContent"
+            element={
+              <>
+                <Header />
+                <MainKidsClothesContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainLivingFurnitureContent"
+            element={
+              <>
+                <Header />
+                <MainLivingFurnitureContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainBedroomFurnitureContent"
+            element={
+              <>
+                <Header />
+                <MainBedroomFurnitureContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainNovelsContent"
+            element={
+              <>
+                <Header />
+                <MainNovelsContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainTextbooksContent"
+            element={
+              <>
+                <Header />
+                <MainTextbooksContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainComicsContent"
+            element={
+              <>
+                <Header />
+                <MainComicsContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainFruitsContent"
+            element={
+              <>
+                <Header />
+                <MainFruitsContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainVegetablesContent"
+            element={
+              <>
+                <Header />
+                <MainVegetablesContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainSnacksContent"
+            element={
+              <>
+                <Header />
+                <MainSnacksContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainBoyToysContent"
+            element={
+              <>
+                <Header />
+                <MainBoyToysContent />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route
+            path="/MainGirlToysContent"
+            element={
+              <>
+                <Header />
+                <MainGirlToysContent />
+                <Footer />
+              </>
+            }
+          ></Route>
         </Routes>
       </BrowserRouter>
-
     );
   }
 }
--- src/client/src/GroceryTypes.js
+++ src/client/src/GroceryTypes.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
-import {Row }from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
-import Aos from "aos";
-import "aos/dist/aos.css";
-import "./GroceryTypes.css";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import './GroceryTypes.css';
+import { Link } from 'react-router-dom';
 
 export default function GroceryTypes() {
   return (
     <Container>
-    
-    <Row>
-    <Col sm><div className ="grocery_types">
-       <img data-aos="fade" src= "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg" alt="Fruits"  ></img>
-       <h3>Fruits</h3>
-       <p>Get the freshest fruits in the market at Taiga</p>
-    </div></Col>
-    <Col sm><div className ="grocery_types">
-        <img data-aos="fade" src="https://cdn.britannica.com/17/196817-050-6A15DAC3/vegetables.jpg" alt="Vegetables" ></img>
-        <h3>Vegetables</h3>
-        <p>Get the freshest vegetables in the market at Taiga</p>
-    </div></Col>
-    <Col sm><div className ="grocery_types">
-        <img data-aos="fade" src= "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/snacks-in-america.jpg?quality=82&strip=all&w=1200" alt="Snacks" ></img>
-        <h3>Snacks</h3>
-        <p>Get exclusive deals on kids favourite snacks here at Taiga</p>
-    </div> </Col>
-</Row>
-</Container>
-  )
+      <Row>
+        <Col sm>
+          <div className="grocery_types">
+            <Link to="/MainFruitsContent">
+              <img
+                data-aos="fade"
+                src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assortment-of-colorful-ripe-tropical-fruits-top-royalty-free-image-995518546-1564092355.jpg"
+                alt="Fruits"
+              ></img>
+            </Link>
+            <h3>Fruits</h3>
+            <p>Get the freshest fruits in the market at Taiga</p>
+          </div>
+        </Col>
+        <Col sm>
+          <div className="grocery_types">
+            <Link to="/MainVegetablesContent">
+              <img
+                data-aos="fade"
+                src="https://cdn.britannica.com/17/196817-050-6A15DAC3/vegetables.jpg"
+                alt="Vegetables"
+              ></img>
+            </Link>
+            <h3>Vegetables</h3>
+            <p>Get the freshest vegetables in the market at Taiga</p>
+          </div>
+        </Col>
+        <Col sm>
+          <div className="grocery_types">
+            <Link to="/MainSnacksContent">
+              <img
+                data-aos="fade"
+                src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/snacks-in-america.jpg?quality=82&strip=all&w=1200"
+                alt="Snacks"
+              ></img>
+            </Link>
+            <h3>Snacks</h3>
+            <p>Get exclusive deals on kids favourite snacks here at Taiga</p>
+          </div>{' '}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
