a37c9d7db312b94e292c6d5f788cccce1be910d9
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-02-17 13:56:03 -0500

    Fetch products for electronics

--- src/client/src/Electronicslist.js
+++ src/client/src/Electronicslist.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
+class Electronicslist extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       posts: []
+    }
+  }
 
-export default function Electronicslist() {
+  componentDidMount(){
+    axios.get("http://localhost:9000/products/search")
+      .then(response => {
+        console.log(response)
+        this.setState({posts: response.data})
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
   
-  
-  
-  return (
-   <div>
-   
-   </div>
-  )
+  render() {
+    const {posts} = this.state
+    return (
+      <div>
+        Electronicslist
+      {
+        posts.length ?
+        posts.map(post => <div key={post.categories}>{post.name}</div>) :
+        null
+      }
+      </div>
+    )
+  }
 }
+
+export default Electronicslist
