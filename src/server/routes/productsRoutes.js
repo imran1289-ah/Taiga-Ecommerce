c8ad3cd35036897ad7c14a3f66048db8c77dadeb
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-03-06 22:18:51 -0500

    Fixing some bugs in api

--- src/server/routes/productsRoutes.js
+++ src/server/routes/productsRoutes.js
@@ -4,35 +4,36 @@ const ProductModel = require('../models/products');
 
 //Adds new product into the database
 router.route('/create').post((req,res) => {
-    ProductModel.save()
-        .then(product => {
-            name: req.body.name
-            description: req.body.description
-            price: req.body.price
-            image: req.body.image
-            stock: req.body.stock
-            categories: req.body.categories
-            user: req.body.user
-            console.log("Product was added into the database");
-        })
-        .catch(error => console.log("Error"))
-})
+    productName = req.body.name;
+    productDescription = req.body.description;
+    productPrice = req.body.price;
+    productImage = req.body.image;
+    productStock = req.body.stock;
+    productCategories = req.body.categories;
+    productUser = req.body.user;
+
+    NewProduct = new ProductModel({productName, productDescription, productPrice, productImage, productStock, productCategories, productUser});
+
+    NewProduct.save()
+        .then(console.log("Product was sucessful added"))
+        .catch(error => console.log(error));
+});
 
 //Edit product into the database
 router.route('/update/:id').post((req,res) => {
     ProductModel.findByIdAndUpdate(req.params.id)
         .then(product => {
-            name: req.body.name
-            description: req.body.description
-            price: req.body.price
-            image: req.body.image
-            stock: req.body.stock
-            categories: req.body.categories
-            user: req.body.user
+            product.name = req.body.name
+            product.description = req.body.description
+            product.price = req.body.price
+            product.image =  req.body.image
+            product.stock = req.body.stock
+            product.categories =  req.body.categories
+            product.user =  req.body.user
 
             product.save()
                 .then(() => console.log("Product was updated in the database"))
-                .catch(error => console.log("Error"));
+                .catch(error => console.log(error));
             
         } )
         .catch(error => res.json("Error finding product"))
