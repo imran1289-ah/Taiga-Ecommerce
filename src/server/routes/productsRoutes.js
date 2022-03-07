79b1a8bebc938294d45e25b75fb944ef625d9f03
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-03-06 20:44:49 -0500

    Adding Comments/Refactoring Api endpoints for seller #52  #51  #50  #24

--- src/server/routes/productsRoutes.js
+++ src/server/routes/productsRoutes.js
@@ -2,46 +2,47 @@ const express = require('express');
 const router = express.Router();
 const ProductModel = require('../models/products');
 
+//Adds new product into the database
+router.create('/create').post((req,res) => {
+    ProductModel.save()
+        .then(product => {
+            name: req.body.name
+            description: req.body.description
+            price: req.body.price
+            image: req.body.image
+            stock: req.body.stock
+            categories: req.body.categories
+            user: req.body.user
+            console.log("Product was added into the database");
+        })
+        .catch(error => console.log("Error"))
+})
 
-router.route('/create').post((req,res) => {
-    const name = req.body.name;
-    const description = req.body.description;
-    const price = req.body.price;
-    const image = req.body.image;
-    const stock = req.body.stock;
-    const categories = req.body.categories;
-    const user = req.body.user;
-
-    const AddedProduct = new ProductModel({name, description, price, image, stock, categories, user});
-
-    AddedProduct.save()
-        .then(() => res.json("Product was sucessful added"))
-        .catch(error => res.json("Error"));
-});
-
+//Edit product into the database
 router.route('/update/:id').post((req,res) => {
     ProductModel.findByIdAndUpdate(req.params.id)
         .then(product => {
-            product.name = req.body.name
-            product.description = req.body.description
-            product.price = req.body.price
-            product.image = req.body.image
-            product.stock = req.body.stock
-            product.categories = req.body.categories
-            product.user = req.body.user
+            name: req.body.name
+            description: req.body.description
+            price: req.body.price
+            image: req.body.image
+            stock: req.body.stock
+            categories: req.body.categories
+            user: req.body.user
 
             product.save()
-                .then(() => res.json("Product was updated"))
-                .catch(error => res.json("Error"));
+                .then(() => console.log("Product was updated in the database"))
+                .catch(error => console.log("Error"));
             
         } )
         .catch(error => res.json("Error finding product"))
 });
 
+//Delete product from the database
 router.route('/delete/:id').delete((req,res) => {
     ProductModel.findByIdAndDelete(req.params.id)
-        .then(() => res.json("Product Deleted"))
-        .catch(error => res.json("Error"));
+        .then(() => console.log("Product Deleted in the database"))
+        .catch(error => console.log("Error"));
 })
 
 module.exports = router;
