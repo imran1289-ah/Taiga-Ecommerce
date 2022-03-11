62d66235d644b1a757d4e11c37eff2b824e181df
MichaelDjab <mich.dja@gmail.com>
2022-03-11 08:57:32 -0500

    Improve login ux #67  #35
    Show when user is logged in or logged out in header
    #67  #35

--- src/client/src/Login.js
+++ src/client/src/Login.js
@@ -32,7 +32,9 @@ const Login = () => {
         }),
       });
       if (res.status === 200) {
-        setMessage("Welcome back " + email);
+        localStorage.setItem("email", email);
+        window.location.reload(false);
+        window.alert("Welcome back");
         setPassword("");
         setEmail("");
       } else if(res.status === 400){
@@ -72,6 +74,7 @@ const Login = () => {
         setREmail("");
         setRPassword("");
         setRName("");
+        window.location.reload(false);
         setRMessage(rName + " registered successfully");
       } else {
         setRMessage("Some error occured with status " + res.status);
@@ -94,6 +97,8 @@ const Login = () => {
           'Content-Type': 'application/json'},
     });
     if (res.status === 200) {
+      localStorage.setItem("email", "Not Logged In");
+      window.location.reload(false);
       setMessage("Logged out with success");
     } else if(res.status === 401){
       setMessage('User not logged in');
@@ -136,7 +141,7 @@ const Login = () => {
                 ></input>
                 <button type="submit" class="login-signup-button">Login</button>
             </form>
-            <div>{message.includes("Welcome back ") ? <button type="logout" class="logout-button" onClick={handleLogout}>Logout</button> : null}</div>
+            <div>{localStorage.email != "Not Logged In" ? <button type="logout" class="logout-button" onClick={handleLogout}>Logout</button> : null}</div>
             <div class="login-message" >{message ? <p>{message}</p> : null}</div>
         </div>
 
